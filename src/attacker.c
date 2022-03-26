8e43399059ae140ac8718c0547402296d9cc512e
Megaboss007 <58306524+Megaboss007@users.noreply.github.com>
2022-03-26 10:51:29 -0300

    Add files via upload

--- src/attacker.c
+++ src/attacker.c
@@ -19,11 +19,58 @@
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+ 
+  // In C, static variables are kept between executions of the function
+  // By construction, the attacker will never be in the INVALID_POSITION,
+  // so this works to initialize the variable.
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t best_direction = DIR_RIGHT;
+  static position_t spypositiondfd = INVALID_POSITION;
+  static int cont = 0;
+  static bool up = false;
+  static bool down = false;
+  static bool stop = false;
+  
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  if (cont == 4) // Aqui pode ser utilizado um número aleatório
+    spypositiondfd = get_spy_position(defender_spy);
+
+  
+if(cont>=4 && !stop) {
+
+  if(spypositiondfd.i > attacker_position.i)   {
+    best_direction = (direction_t) DIR_UP_LEFT;
+    up = true;    
+    }
+  else {
+    best_direction = (direction_t) DIR_DOWN_LEFT; 
+    down = true;
+    }
 }
+  else if (stop) {
+  if(up)
+  best_direction = (direction_t)  DIR_DOWN_RIGHT;
+    else if (down) 
+    best_direction = (direction_t)  DIR_UP_RIGHT;
+  }
+
+  if (equal_positions(attacker_position, previous_position) ) {
+    if (up)
+    best_direction = (direction_t)  DIR_DOWN_RIGHT;
+    else if (down)
+    best_direction = (direction_t)  DIR_UP_RIGHT;
+    else
+    best_direction = (direction_t) DIR_RIGHT;
+    stop = true;
+    up = false;
+    down = false;
+  } 
+
+
+  previous_position = attacker_position;
+  cont++;
+  return best_direction;
+}
+
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -6,11 +6,13 @@
 #include "position.h"
 #include "spy.h"
 
+
 // Main header
 #include "defender.h"
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -19,11 +21,37 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  
+// In C, static variables are kept between executions of the function
+  // By construction, the attacker will never be in the INVALID_POSITION,
+  // so this works to initialize the variable.
+ 
+  static position_t previous_position = INVALID_POSITION;
+  static position_t spypositionatk = INVALID_POSITION;
+  
+  static direction_t best_direction = DIR_UP;
+  static direction_t directionaux= DIR_DOWN;
+  static direction_t directionaux2= DIR_UP;
+  static int cont = 0; 
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+if(cont == 10) // Aqui pode ser utilizado um número aleatório
+  spypositionatk  = get_spy_position(attacker_spy);
+   if(get_spy_number_uses(attacker_spy) != 0){
+   if(spypositionatk.i > defender_position.i)   
+    best_direction = (direction_t) DIR_DOWN;
+  else if (spypositionatk.i < defender_position.i)
+    best_direction = (direction_t) DIR_UP;
+  else best_direction = (direction_t) DIR_STAY;
+   }
+  if (equal_positions(defender_position, previous_position)) {
+   best_direction = directionaux;
+   directionaux = directionaux2;
+   directionaux2 = best_direction; 
+  }     
+  previous_position = defender_position;
+  cont++;
+  return best_direction;
+
 }
 
 /*----------------------------------------------------------------------------*/
